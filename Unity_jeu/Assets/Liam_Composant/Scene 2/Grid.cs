7b3a3c4e9b777abde4519dc9d5392276dff21b0a
Liam <frit.liam1@gmail.com>
2025-10-06 15:47:06 +0200

    Optimisation du pathfinding

--- Unity_jeu/Assets/Liam_Composant/Scene 2/Grid.cs
+++ Unity_jeu/Assets/Liam_Composant/Scene 2/Grid.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class Grid : MonoBehaviour
 {
+    public bool onlyDisplayPathGizmos;
     public Transform player;
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
@@ -20,6 +21,14 @@ public class Grid : MonoBehaviour
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
         CreateGrid();
+    }
+
+    public int MaxSize
+    {
+        get
+        {
+            return gridSizeX * gridSizeY;
+        }
     }
 
     void CreateGrid()
@@ -51,7 +60,7 @@ public class Grid : MonoBehaviour
                 int checkX = node.gridX + x;
                 int checkY = node.gridY + y;
 
-                if(checkX >= 0 && checkY < gridSizeX && checkY >=0 && checkY < gridSizeY)
+                if(checkX >= 0 && checkX < gridSizeX && checkY >=0 && checkY < gridSizeY)
                 {
                     neighbours.Add(grid[checkX, checkY]);
                 }
@@ -78,18 +87,33 @@ public class Grid : MonoBehaviour
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if (grid != null)
+        if (onlyDisplayPathGizmos)
         {
-            Node playerNode = NodeFromWorldPoint(player.position);
-            foreach (Node n in grid)
+            if (path != null)
             {
-                Gizmos.color = (n.walkable) ? Color.white : Color.red;
-                if (path != null)
-                    if (path.Contains(n))
-                        Gizmos.color = Color.black;
-                if (n == playerNode)
-                    Gizmos.color = Color.cyan;
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                foreach (Node n in path)
+                {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
+            }
+        }
+        else
+        {
+
+            if (grid != null)
+            {
+                Node playerNode = NodeFromWorldPoint(player.position);
+                foreach (Node n in grid)
+                {
+                    Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                    if (path != null)
+                        if (path.Contains(n))
+                            Gizmos.color = Color.black;
+                    if (n == playerNode)
+                        Gizmos.color = Color.cyan;
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
             }
         }
 
--- Unity_jeu/Assets/Liam_Composant/Scene 2/Node.cs
+++ Unity_jeu/Assets/Liam_Composant/Scene 2/Node.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Node
+public class Node : IHeapItem<Node>
 {
     public bool walkable;
     public Vector3 worldPosition;
@@ -11,6 +11,8 @@ public class Node
     public int gCost;
     public int hCost;
     public Node parent;
+
+    int heapIndex;
 
     public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
     {
@@ -25,5 +27,27 @@ public class Node
     public int fCost
     {
         get { return gCost + hCost; }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return -compare;
     }
 }
